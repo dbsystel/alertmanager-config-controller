3697c49e3141d3c180158b31d72a772ab7a56202
Kirchen99 <latias_latios@126.com>
2019-05-08 11:08:36 +0200

    add continue: true if it is not exist in routes

--- controller/controller.go
+++ controller/controller.go
@@ -14,6 +14,7 @@ import (
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	alcf "github.com/prometheus/alertmanager/config"
+	"gopkg.in/yaml.v2"
 	"k8s.io/api/core/v1"
 )
 
@@ -256,7 +257,7 @@ func (c *Controller) createConfig(configmapObj *v1.ConfigMap) {
 	if _, err = os.Stat(path); os.IsNotExist(err) {
 		err = os.MkdirAll(path, 0766)
 		if err != nil {
-			level.Error(c.logger).Log("msg", "Failed to create directory", "err", err)
+			level.Error(c.logger).Log("msg", "Failed to create directory", "err", err.Error())
 		}
 	}
 
@@ -269,6 +270,12 @@ func (c *Controller) createConfig(configmapObj *v1.ConfigMap) {
 		} else {
 			filename = configmapObj.Namespace + "-" + configmapObj.Name + "-" + k
 		}
+
+		if typ == "route" {
+			v = c.addContinueIfNotExist(v)
+		}
+
+
 
 		level.Info(c.logger).Log(
 			"msg", "Creating " + typ + ": " + k,
@@ -286,6 +293,26 @@ func (c *Controller) createConfig(configmapObj *v1.ConfigMap) {
 	}
 }
 
+func (c *Controller) addContinueIfNotExist(routeString string) string {
+	m := make([]map[string]interface{}, 1, 1)
+
+	err := yaml.Unmarshal([]byte(routeString), &m)
+	if err != nil {
+		level.Error(c.logger).Log("msg", "Format error in route string: " + routeString, "err", err.Error())
+	}
+
+	for _, route := range m {
+		route["continue"] = true
+	}
+
+	v, err := yaml.Marshal(&m)
+	if err != nil {
+		level.Error(c.logger).Log("msg", "Format error in route yaml", "err", err.Error())
+	}
+
+	return string(v)
+}
+
 // backup currently unavailable configs for further usage
 func (c *Controller) createBackfile(configmapObj *v1.ConfigMap, typ string) {
 	var err error
@@ -294,11 +321,12 @@ func (c *Controller) createBackfile(configmapObj *v1.ConfigMap, typ string) {
 		if _, err = os.Stat(path); os.IsNotExist(err) {
 			err = os.MkdirAll(path, 0766)
 			if err != nil {
-				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err)
+				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err.Error())
 			}
 		}
 		for k, v := range configmapObj.Data {
 			filename := configmapObj.Namespace + "-" + configmapObj.Name + "-" + k
+			v = c.addContinueIfNotExist(v)
 			level.Debug(c.logger).Log(
 				"msg", "Backup route: " + k + ", and waiting for receiver",
 				"namespace", configmapObj.Namespace,
@@ -314,7 +342,7 @@ func (c *Controller) createBackfile(configmapObj *v1.ConfigMap, typ string) {
 		if _, err = os.Stat(path); os.IsNotExist(err) {
 			err = os.MkdirAll(path, 0766)
 			if err != nil {
-				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err)
+				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err.Error())
 			}
 		}
 		for k, v := range configmapObj.Data {
@@ -334,7 +362,7 @@ func (c *Controller) createBackfile(configmapObj *v1.ConfigMap, typ string) {
 		if _, err = os.Stat(path); os.IsNotExist(err) {
 			err = os.MkdirAll(path, 0766)
 			if err != nil {
-				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err)
+				level.Error(c.logger).Log("msg", "Failed to create backup directory", "err", err.Error())
 			}
 		}
 		for k, v := range configmapObj.Data {
